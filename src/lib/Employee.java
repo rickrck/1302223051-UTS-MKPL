e11b71957118743125ef38b3f0d6a4d596b1f9f1
Ricky Renaldi <109163100+rickrck@users.noreply.github.com>
2025-04-19 05:18:48 +0700

    refactor(employee): Make class called Person
    Memisahkan attribute dari employee ke dalam class Person.
    
    Signed-off-by: Ricky Renaldi <109163100+rickrck@users.noreply.github.com>

--- src/lib/Employee.java
+++ src/lib/Employee.java
@@ -5,21 +5,9 @@ import java.time.Month;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Employee {
+public class Employee extends Person {
 
-	private String employeeId;
-	private String firstName;
-	private String lastName;
-	private String idNumber;
-	private String address;
-	
-	private int yearJoined;
-	private int monthJoined;
-	private int dayJoined;
-	private int monthWorkingInYear;
-	
-	private boolean isForeigner;
-	private boolean gender; //true = Laki-laki, false = Perempuan
+	private Person person;
 	
 	private int monthlySalary;
 	private int otherMonthlyIncome;
@@ -31,18 +19,9 @@ public class Employee {
 	private List<String> childNames;
 	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, boolean gender) {
-		this.employeeId = employeeId;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.idNumber = idNumber;
-		this.address = address;
-		this.yearJoined = yearJoined;
-		this.monthJoined = monthJoined;
-		this.dayJoined = dayJoined;
-		this.isForeigner = isForeigner;
-		this.gender = gender;
-		
+	public Employee(Person person) {
+		this.person = person;
+
 		childNames = new LinkedList<String>();
 		childIdNumbers = new LinkedList<String>();
 	}
---
+++ src/lib/Person.java
@@ -0,0 +1,84 @@
+package lib;
+
+import java.time.LocalDate;
+
+public class DataEmployee {
+    private String employeeId;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String address;
+
+    private int yearJoined;
+    private int monthJoined;
+    private int dayJoined;
+    private int monthWorkingInYear;
+
+    private boolean isForeigner;
+    private boolean gender; //true = Laki-laki, false = Perempuan
+
+    public DataEmployee(String employeeId, String firstName, String lastName, String idNumber, String address, int yearJoined, int monthJoined, int dayJoined, int monthWorkingInYear, boolean isForeigner, boolean gender) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.address = address;
+        this.yearJoined = yearJoined;
+        this.monthJoined = monthJoined;
+        this.dayJoined = dayJoined;
+        this.monthWorkingInYear = monthWorkingInYear;
+        this.isForeigner = isForeigner;
+        this.gender = gender;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getYearJoined() {
+        return yearJoined;
+    }
+
+    public int getMonthJoined() {
+        return monthJoined;
+    }
+
+    public int getDayJoined() {
+        return dayJoined;
+    }
+
+    public int getMonthWorkingInYear() {
+        LocalDate date = LocalDate.now();
+        if (date.getYear() == yearJoined) {
+            monthWorkingInYear = date.getMonthValue() - monthJoined;
+        } else {
+            monthWorkingInYear = 12;
+        }
+        return monthWorkingInYear;
+    }
+
+    public boolean isForeigner() {
+        return isForeigner;
+    }
+
+    public boolean isGender() {
+        return isGender();
+    }
+
+}
